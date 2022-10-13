f1d9f99e9f450236b74fa7754fd083d5fa3f3cc8
SayZ <derrik_cool@mail.ru>
2022-10-13 18:48:45 +0300

    Init
    Initial commit

---
+++ TViDR_UTest/TViDR_UTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using datacompression;
+
+namespace TViDR_UTest
+{
+    [TestClass]
+    public class DataCompressionTest
+    {
+        [TestMethod]
+        public void AlphabetTest()
+        {
+            MTF mTF = new MTF();
+        }
+    }
+}
